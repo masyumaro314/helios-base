acc4088d95191c60922cbec264815eb39832bea8
masyumaro314 <ramanujan.haruki@gmail.com>
2026-02-12 08:27:35 +0900

    Update changes

---
+++ build/config.h
@@ -0,0 +1,7 @@
+
+
+#define PACKAGE_NAME "helios-base"
+
+#define VERSION 2023.3
+
+#define HAVE_LIBZ
---
+++ build/src/trainer/CMakeFiles/sample_trainer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../bin/sample_trainer"
+  "../../bin/sample_trainer.pdb"
+  "CMakeFiles/sample_trainer.dir/main_trainer.cpp.o"
+  "CMakeFiles/sample_trainer.dir/main_trainer.cpp.o.d"
+  "CMakeFiles/sample_trainer.dir/sample_trainer.cpp.o"
+  "CMakeFiles/sample_trainer.dir/sample_trainer.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_trainer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
